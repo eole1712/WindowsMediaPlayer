4929b4d325796ad834dfe5692fc5400b57fa332e
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2016-01-09 13:27:04 +0100

    Ajout du namespace

--- MyWindowsMediaPlayer/VideoFlux.vb
+++ MyWindowsMediaPlayer/VideoFlux.vb
@@ -1,18 +1,20 @@
-﻿Public Class VideoFlux
-    Private Property mp As MediaPlayer
-    Private Property vd As VideoDrawing
-    Private Property db As DrawingBrush
+﻿Namespace MyWindowsMediaPlayer
+    Public Class VideoFlux
+        Private Property mp As MediaPlayer
+        Private Property vd As VideoDrawing
+        Private Property db As DrawingBrush
 
-    Sub New(ByVal file As String)
-        Dim mp As MediaPlayer = New MediaPlayer()
-        mp.Open(New Uri(file))
+        Sub New(ByVal file As String)
+            Dim mp As MediaPlayer = New MediaPlayer()
+            mp.Open(New Uri(file))
 
-        vd.Player = mp
-        vd.Rect = New Rect(0, 0, 200, 200)
+            vd.Player = mp
+            vd.Rect = New Rect(0, 0, 200, 200)
 
-        'Binding videobrush
-        'MediaElement. = db
+            'Binding videobrush
+            'MediaElement. = db
 
-        mp.Play()
-    End Sub
-End Class
+            mp.Play()
+        End Sub
+    End Class
+End Namespace
