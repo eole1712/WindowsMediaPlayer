04223caf163fe2cf67e17a0050dfd16a21f39609
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2016-01-09 13:24:42 +0100

    Début de la classe VideoFlux

---
+++ MyWindowsMediaPlayer/VideoFlux.vb
@@ -0,0 +1,18 @@
+﻿Public Class VideoFlux
+    Private Property mp As MediaPlayer
+    Private Property vd As VideoDrawing
+    Private Property db As DrawingBrush
+
+    Sub New(ByVal file As String)
+        Dim mp As MediaPlayer = New MediaPlayer()
+        mp.Open(New Uri(file))
+
+        vd.Player = mp
+        vd.Rect = New Rect(0, 0, 200, 200)
+
+        'Binding videobrush
+        'MediaElement. = db
+
+        mp.Play()
+    End Sub
+End Class
